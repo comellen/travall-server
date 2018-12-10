dfd1ac5194d120840a089aee64aa50d2fc375dca
Connor Mellencamp <connorsmac@Connors-MacBook-Pro.local>
2018-12-10 11:21:00 -0500

    added activity model

---
+++ models/activity.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('activity', {
+       title: {
+           type: DataTypes.STRING,
+           allowNull: false,
+           validate: {
+               isAlphanumeric: true
+           },
+        },
+        date: {
+            type: DataTypes.DATEONLY,
+        },
+        startTime: {
+            type: DataTypes.STRING,
+        },
+        endTime: {
+            type: DataTypes.STRING,
+        },
+        location: {
+            type: DataTypes.STRING,
+        },
+        description: {
+            type: DataTypes.STRING,
+        },
+    });
+};
