3696f2749ef71e70f6c6c2e85284aac1888d6d41
Connor Mellencamp <connorsmac@Connors-MacBook-Pro.local>
2018-12-12 15:51:11 -0500

    herro

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -22,6 +22,7 @@ router.post('/login', (req, res) => {
     User.findOne({ where: { username: req.body.username } })
         .then(user => {
             if (user) {
+                console.log('herro derr')
                 bcrypt.compare(req.body.password, user.password, (err, matches) => {
                     if (matches) {
                         let token = jwt.sign({ id: user.id }, process.env.SECRET, { expiresIn: 60 * 60 * 24 });
