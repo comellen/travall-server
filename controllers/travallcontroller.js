0af404ab3e900a5e054a66674a567717b1e5716a
Connor Mellencamp <connorsmac@Connors-MacBook-Pro.local>
2018-12-14 14:25:36 -0500

    added correct userid grab

--- controllers/travallcontroller.js
+++ controllers/travallcontroller.js
@@ -12,7 +12,7 @@ router.post('/create', (req, res) => {
         endDate: req.body.endDate
     })
         .then(travall => {
-            travall.addUser(req.user.id);
+            travall.addUser(req.body.user.id);
             res.json({ newtravall: travall });
         },
             err => { res.send(500, err.message); });
