ffc2419b1471004e2a82ff5b2534a84b7c5d5416
Connor Mellencamp <connorsmac@Connors-MacBook-Pro.local>
2018-12-16 15:06:21 -0500

    added admin signup

--- controllers/admincontroller.js
+++ controllers/admincontroller.js
@@ -4,6 +4,24 @@ const Travall = require('../db').import('../models/travall');
 const User = require('../db').import('../models/user');
 const Admin = require('../db').import('../models/admin');
 
+router.post('/signup', (req, res) => {
+    Admin.create({
+        username: req.body.username,
+        password: bcrypt.hashSync(req.body.password, 10),
+    })
+        .then(
+            admin => {
+                let token = jwt.sign({ id: admin.id }, process.env.SECRET, { expiresIn: 60 * 60 * 24 });
+                res.json({
+                    admin: admin,
+                    message: 'Admin created',
+                    sessionToken: token
+                });
+            },
+            err => res.send(500, err)
+        );
+});
+
 router.post('/login', (req, res) => {
     Admin.findOne({ where: { username: req.body.username } })
         .then(user => {
